db432d305f828e7062c3898a83e71af3a8253d62
fededdileo <fdileo97@gmail.com>
2021-12-07 13:33:01 -0300

    map implementation and operation

---
+++ src/main/resources/static/scripts/localization.js
@@ -0,0 +1,14 @@
+class Localization {
+    constructor(callback){
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition((position)=>{
+                this.latitude = position.coords.latitude;
+                this.longitude = position.coords.longitude;
+
+                callback();
+            });
+        }else{
+            alert("Tu navegador no soporta geolocalizacion!!")
+        }
+    }
+}
