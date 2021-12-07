9c8cfde0be46e7b70b0b374f57e77d30263ee249
Brito Maria Luz <britomluz@gmail.com>
2021-12-07 09:39:22 -0300

    login and register page, add categories order.js, complete order.html

--- src/main/resources/static/scripts/order.js
+++ src/main/resources/static/scripts/order.js
@@ -2,81 +2,106 @@ const app = Vue.createApp({
   data() {
     return {
       products: [],
-      productId:"",
-      productName:"",
-      productImage:"",
+      productId: "",
+      productName: "",
+      productImage: "",
       price: "",
       stock: [],
-      ordername:"",
-      orderquantity:0,
-      priceorder:0,
-      totalorder:0
+      ordername: "",
+      orderquantity: 0,
+      priceorder: 0,
+      totalorder: 0,
+      categories:[],
+
+      //product categories
+      mainCourse:[],
+      entriesSnacks:[],
+      specials:[],
+      chefPicks:[],
+      soup:[],
+      drinks:[],
+      pasta:[],      
+      vegetarian:[],
+      salads:[],
     }
   },
   created() {
-    axios.get("/api/products")
-    .then(response => {
-      console.log(response)
-      this.products = response.data
-    })
-    .catch(err=>console.log(err))
-    this.Admin_accept_order();
+    //this.Admin_accept_order()
+    this.loadProducts()
+    this.loadCategories()
+
   },
   methods: {
-      createProduct(){
-        axios.post('/api/products/create', idCategory = productId,{//ren, I added {} because I got an error 
-
-          name : this.productName,
-          description : this.description, 
-          productImage : this.productImage,
-          price : this.price,
-          stock : this.stock
+    loadProducts() {
+      axios.get('/api/products')
+        .then(response => {         
+          this.products = response.data.products
+          //this.mainCourse = this.products.filter(product => product.categories)
         })
-          .then(console.log("Product created"))
-          .catch(err=>console.log(err))
-      },
-      deleteProduct(){
-        axios.delete(`/api/products/delete/${this.id}`)
+       .catch(err => console.log(err.response.data))
+    },
+    loadCategories(){
+      axios.get('/api/categories')
+      .then(response => {         
+        this.categories = response.data.categories  
+        console.log(this.categories)      
+      })
+     .catch(err => console.log(err.response.data))
+    },
+    createProduct() {
+      axios.post('/api/products/create', idCategory = productId, {//ren, I added {} because I got an error 
+
+        name: this.productName,
+        description: this.description,
+        productImage: this.productImage,
+        price: this.price,
+        stock: this.stock
+      })
+        .then(console.log("Product created"))
+        .catch(err => console.log(err))
+    },
+    deleteProduct() {
+      axios.delete(`/api/products/delete/${this.id}`)
         .then(console.log("Product delete"))
-        .catch(err=>console.log(err))
-      },
-      editProduct(){
-        axios.patch(`/api/products/edit/${this.id}`, {//ren, I added {} because I got an error 
-          name : this.productName, 
-          description : this.description, 
-          productImage : this.productImage, 
-          price : this.price,
-          stock : this.stock
-        }
-        )
+        .catch(err => console.log(err))
+    },
+    editProduct() {
+      axios.patch(`/api/products/edit/${this.id}`, {//ren, I added {} because I got an error 
+        name: this.productName,
+        description: this.description,
+        productImage: this.productImage,
+        price: this.price,
+        stock: this.stock
+      }
+      )
         .then(console.log("Product Edit"))
-        .catch(err=>console.log(err))
-      }, 
+        .catch(err => console.log(err))
+    },
 
-    Edit_order(){
-        axios.patch(`/api/order/edit/${this.idOrder}`)
-        .then(response=>console.log(response))
-        .catch(err=>console.log(err))
+    Edit_order() {
+      axios.patch(`/api/order/edit/${this.idOrder}`)
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
     },
-    Delete_order(){
+    Delete_order() {
       axios.delete(`/api/order/cancel/${this.idOrder}`)
-      .then(response=>console.log(response))
-      .catch(err=>console.log(err))
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
     },
-    Admin_accept_order(){
+    Admin_accept_order() {
       axios.get(`/api/order/confirm/${this.idOrder}`)
-      .then(response=>console.log(response))
-        .catch(err=>console.log(err))
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
     },
-    user_Accept_order(){
-        axios.post("/api/order/buy",{
-          name: this.ordername,
-          quantity: this.orderquantity,
-          price:this.priceorder,
-          total: this.totalorder
-        })
-        .then(response=>console.log(response))
-        .catch(err=>console.log(err))
+    user_Accept_order() {
+      axios.post("/api/order/buy", {
+        name: this.ordername,
+        quantity: this.orderquantity,
+        price: this.priceorder,
+        total: this.totalorder
+      })
+        .then(response => console.log(response))
+        .catch(err => console.log(err))
     }
   }
 })
