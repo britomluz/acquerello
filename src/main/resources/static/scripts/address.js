4d6b8958f2509507d8945a97f2e4c2d5fd57e6fc
renzo <renzoserrano@outlook.com>
2021-12-09 13:03:54 -0300

    fix order js and add adress

--- src/main/resources/static/scripts/address.js
+++ src/main/resources/static/scripts/address.js
@@ -1,40 +1,50 @@
 const App = Vue.createApp({
   data() {
     return {
-      street:"",
-      numbetAddres:0,
-      zip:"",
-      state:"",
-      reference:"",
+      street: "",
+      numberAddress: 0,
+      zip: "",
+      state: "",
+      reference: "",
+      addrefence: "",
+      addstate: "",
+      addzip: "",
+      addnumber: 0,
+      addstreet: "",
     };
   },
   created() {},
   methods: {
-    send_address(e) {
-      e.preventDefault()
+    send_address() {
       axios
-        .post("/address/create", {
+        .post("/api/address/create", {
           street: this.street,
-          number: this.numbetAddres,
+          number: this.numberAddress,
           zip: this.zip,
           state: this.state,
-          reference:this.reference,
+          reference: this.reference,
         })
         .then((response) => alert(response.data))
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err.response.data));
     },
     edit_address() {
       axios
-        .patch()
+        .patch(`/api/address/edit?id=${1}`, {
+          street: this.addstreet,
+          number: this.addnumber,
+          zip: this.addzip,
+          state: this.addstate,
+          reference: this.addrefence,
+        })
         .then((response) => console.log(response))
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err.response.data));
     },
     deLete_address() {
       axios
-        .delete()
+        .delete(`/api/address/delete?id=${2}`)
         .then((response) => console.log(response))
-        .catch((err) => console.log(err));
+        .catch((err) => console.log(err.response.data));
     },
   },
 });
-App.mount("#app");
+let obv =App.mount("#app");
